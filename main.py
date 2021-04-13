a60d98fe0178422e5ad57188d59aa8edee5acaaa
David-97-dev <66362273+David-97-dev@users.noreply.github.com>
2021-04-13 17:11:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(15, GPIO.IN)
+while True:
+    i = GPIO.input(15)
+    if i == 0:
+        print("No motion", i)
+        time.sleep(0.1)
+    elif i == 1:
+        print("Motion Detected", i)
+        time.sleep(6)
+
